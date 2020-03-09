5a0677aedd9a84fe96e005d25f23893e837a19c7
angel <angelsb94@gmail.com>
2020-03-08 18:11:39 -0600

    inicio de repaso de vue y mejora de la app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Hello word!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
