a147f590d5d982163d852369a6faea6d74549a75
angel <angelsb94@gmail.com>
2020-03-09 19:20:50 -0600

    cambios de traks

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template lang='pug'>
   #app
-    input(v-model="name")
-    button(v-on:click="format") Format(v-on)
-    button(@click="format") Format(reducion de v-on)
-    p {{ formatName }}
-
+    section.large
+      nav.navbar
+        .container
+          .input-group
+            input.form-control(
+              type="text"
+              placeholder='Buscar Canciones'
+              v-model="searchQuery")
+            .input-group-append
+              a.btn.btn-primary(@click="search") buscar
+              a.btn.btn-danger x
+            p {{ searchMessage }}
+      .container
+        .row
+          .col(v-for="t in tracks") {{ t.name }} - {{ t.artis }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'muchacha', artis: 'luis alberto' },
+  { name: 'hoy aca en el baile', artis: 'el pepo' },
+  { name: 'i was made for loving', artis: 'kiss' }]
+
 export default {
   name: 'app',
   data () {
     return {
-      name: '',
-      formatName: ''
+      searchQuery: '',
+      tracks: []
+    }
+  },
+
+  computed: {
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
 
   methods: {
-    format () {
-      this.formatName = this.name.split(' ').join('-').toUpperCase()
+    search () {
+      this.tracks = tracks
     }
   }
 }
